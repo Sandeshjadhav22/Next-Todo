a76d40cd6a59f40c23a6595de779fb7ce739a49e
sandy <sandeshjadhav2002@gmail.com>
2024-03-29 13:10:20 +0530

    [Feat]: Db connected

--- app/api/route.js
+++ app/api/route.js
@@ -1,10 +1,12 @@
 import { ConnectDb } from "@/lib/config/db";
 import { NextResponse } from "next/server";
+import dotenv from "dotenv";
+dotenv.config()
 
 const LoadDB = async() => {
     await ConnectDb()
 }
-LoadDB()  
+LoadDB()   
  
 
 export async function GET(request){
